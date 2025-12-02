0b2c143ee2434bf86044fab5c2e177b870a6c50f
tobeprince(ornot) <mervynsikjr@gmail.com>
2025-12-02 15:28:10 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Valentine? 💝",
+    pageTitle: "Will You Be My Girlfriend? 👸",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
@@ -22,36 +22,36 @@ const CONFIG = {
     // Customize each question and its possible responses
     questions: {
         first: {
-            text: "Do you like me?",                                    // First interaction
+            text: "Does princess like bao?",                                    // First interaction
             yesBtn: "Yes",                                             // Text for "Yes" button
             noBtn: "No",                                               // Text for "No" button
-            secretAnswer: "I don't like you, I love you! ❤️"           // Secret hover message
+            secretAnswer: "(Over here you cutie) Yes ofc bao mi love you! ❤️"           // Secret hover message
         },
         second: {
-            text: "How much do you love me?",                          // For the love meter
+            text: "How much princess loves kokbao?",                          // For the love meter
             startText: "This much!",                                   // Text before the percentage
             nextBtn: "Next ❤️"                                         // Text for the next button
         },
         third: {
-            text: "Will you be my Valentine on February 14th, 2025? 🌹", // The big question!
-            yesBtn: "Yes!",                                             // Text for "Yes" button
-            noBtn: "No"                                                 // Text for "No" button
+            text: "Will you be my Girlfriend? 🌹", // The big question!
+            yesBtn: "es, mi princess accepts bao as her boyfriend,                                             // Text for "Yes" button
+            noBtn: "No, mi is waiting for God to give me the right guy :("                                                 // Text for "No" button
         }
     },
 
     // Love meter messages
     // They show up depending on how far they slide the meter
     loveMessages: {
-        extreme: "WOOOOW You love me that much?? 🥰🚀💝",  // Shows when they go past 5000%
-        high: "To infinity and beyond! 🚀💝",              // Shows when they go past 1000%
-        normal: "And beyond! 🥰"                           // Shows when they go past 100%
+        extreme: "Aiyooo bao me luv you as much too 🥰🚀💝",  // Shows when they go past 5000%
+        high: "BAOOOO STAPH ME SHY 🚀💝",              // Shows when they go past 1000%
+        normal: "WAO BAO 🥰"                           // Shows when they go past 100%
     },
 
     // Messages that appear after they say "Yes!"
     celebration: {
-        title: "Yay! I'm the luckiest person in the world! 🎉💝💖💝💓",
-        message: "Now come get your gift, a big warm hug and a huge kiss!",
-        emojis: "🎁💖🤗💝💋❤️💕"  // These will bounce around
+        title: "Yay! I'm the luckiest guy in the world to have been accepted by this princess! 🎉💝💖💝💓",
+        message: "Bao hope you don't find this cringe cuz me is dying inside when creating this ><",
+        emojis: "🐰👸🤗🍅🙌🏻💕"  // These will bounce around
     },
 
     // Color scheme for the website
