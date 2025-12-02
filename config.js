d522bc5ef5414af1be42ebbf86ac47a549dda593
tobeprince(ornot) <mervynsikjr@gmail.com>
2025-12-02 14:39:55 +0800

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ const CONFIG = {
 
     // The title that appears in the browser tab
     // You can use emojis! 💝 💖 💗 💓 💞 💕
-    pageTitle: "Will You Be My Girlfriend? 💝",
+    pageTitle: "Will You Be My Girlfriend? 👸",
 
     // Floating emojis that appear in the background
     // Find more emojis at: https://emojipedia.org
